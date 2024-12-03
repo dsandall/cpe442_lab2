2e4157764f01356fa44874b7f182c270b9753e69
thebu <dsandalledu@gmail.com>
2024-12-02 17:39:43 -0800

    final?

--- send.sh
+++ send.sh
@@ -4,24 +4,15 @@
 IP_0="10.0.1.154"
 IP_1="10.0.1.17" 
 IP_2="10.0.1.156"
+
+IP_3="10.0.1.20"
+IP_4="10.0.1.23"
+IP_7="10.0.1.21"
+
 IP_ICEPI="10.0.1.152"
 
-# #debug or release
-# PROFILE="dev"
-
-# # Build the binaries
-# cargo build --bin lab6_host --profile $PROFILE 
-# cargo build --bin lab6_compute --profile $PROFILE
-# cargo build --bin lab5_simd --profile $PROFILE
-
-# # SCP commands
-# scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} admin@$IP_ICEPI:/home/admin/
-# scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_0:/home/thebu/
-# scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_1:/home/thebu/
-# scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_2:/home/thebu/
-
-# debug or release
-PROFILE="release"
+#debug or release
+PROFILE="dev"
 
 # Build the binaries
 cargo build --bin lab6_host --profile $PROFILE 
@@ -29,7 +20,24 @@ cargo build --bin lab6_compute --profile $PROFILE
 cargo build --bin lab5_simd --profile $PROFILE
 
 # SCP commands
-scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} admin@$IP_ICEPI:/home/admin/
-scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_1:/home/thebu/
-scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_2:/home/thebu/
-scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_0:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} admin@$IP_ICEPI:/home/admin/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_0:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_1:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_2:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_3:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_4:/home/thebu/
+scp ./target/aarch64-unknown-linux-gnu/debug/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_7:/home/thebu/
+
+# # debug or release
+# PROFILE="release"
+
+# # Build the binaries
+# cargo build --bin lab6_host --profile $PROFILE 
+# cargo build --bin lab6_compute --profile $PROFILE
+# cargo build --bin lab5_simd --profile $PROFILE
+
+# # SCP commands
+# scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} admin@$IP_ICEPI:/home/admin/
+# scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_1:/home/thebu/
+# scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_2:/home/thebu/
+# scp ./target/aarch64-unknown-linux-gnu/release/{lab6_host,lab6_compute,lab5_simd} thebu@$IP_0:/home/thebu/
